eee7a5da3d41bad7e5c3c78010738aa9e2b5713d
Slazor <754107+slazor@users.noreply.github.com>
2021-12-10 19:17:15 +0100

    refactor: sidebar and hunting menu

--- src/render/pages/hunting.jsx
+++ src/render/pages/hunting.jsx
@@ -1,127 +1,53 @@
 import React, { useEffect, useState, useCallback } from 'react';
 
-import HuntingMenu from '../components/hunting-menu';
+import Sidebar from '../components/sidebar';
 import Current from './hunting/current';
 import History from './hunting/history';
 
 const Hunting = () => {
   const [isLogReaderActive, setIsLogReaderActive] = useState(false);
-  const [isSessionModalActive, setIsSessionModalActive] = useState(false);
-  const [isInstanceModalActive, setIsInstanceModalActive] = useState(false);
   const [activeSection, setActiveSection] = useState('current');
-  const [sidebarFull, setSidebarFull] = useState(false);
-  const [activeSession, setActiveSession] = useState({ id: null, instanceId: null });
 
   useEffect(() => {
-    const sessionNewData = newSessionData => {
-      setActiveSession({ id: newSessionData?.id, instanceId: newSessionData?.instanceId });
+    const sessionChange = () => {
+      setActiveSection('current');
     };
 
     const removeLoggerListener = window.api.on('logger-status-changed', status => {
       setIsLogReaderActive(status === 'enabled');
     });
 
-    const removeSessionNewLoggerListener = window.api.on('session-new', sessionNewData);
-    const removeInstanceNewLoggerListener = window.api.on('instance-new', sessionNewData);
-
-    window.api.get('active-session').then(sessionNewData);
+    const removeSessionNewLoggerListener = window.api.on('session-new', sessionChange);
+    const removeInstanceNewLoggerListener = window.api.on('instance-new', sessionChange);
+    const removeInstanceLoadedLoggerListener = window.api.on('instance-loaded', sessionChange);
 
     window.api.get('logreader-status').then(status => {
       setIsLogReaderActive(status === 'enabled');
-    });
-
-    window.api.get('settings').then(settings => {
-      setSidebarFull(settings?.sidebarStyle === 'full');
     });
 
     return () => {
       removeLoggerListener();
       removeSessionNewLoggerListener();
       removeInstanceNewLoggerListener();
+      removeInstanceLoadedLoggerListener();
     };
   }, []);
 
-  const startNewSession = useCallback(() => {
-    window.api.call('new-session');
-    setIsSessionModalActive(false);
-    setActiveSection('current');
-  }, []);
-
-  const startNewInstance = useCallback(() => {
-    window.api.call('load-instance', {
-      sessionId: activeSession?.id,
-      instanceId: 'new',
-    });
-    setIsInstanceModalActive(false);
-    setActiveSection('current');
-  }, [activeSession?.id]);
-
   const loadSessionInstance = useCallback((sessionId, instanceId) => {
     window.api.call('load-instance', { sessionId, instanceId });
-    setIsSessionModalActive(false);
     setActiveSection('current');
   }, []);
-
-  const toggleStreamWindow = useCallback(() => {
-    window.api.call('stream-window-toggle');
-  }, []);
-
-  const toggleSidebar = useCallback(() => {
-    const newStyle = sidebarFull ? 'mini' : 'full';
-    window.api.set('settings', [{ name: 'sidebarStyle', value: newStyle }]).then(settings => {
-      setSidebarFull(settings?.sidebarStyle === 'full');
-    });
-  }, [sidebarFull]);
 
   return (
     <>
-      <div className={`sidebar has-background-grey-darker${sidebarFull ? '' : ' mini'}`}>
-        <HuntingMenu
-          active={activeSection}
-          isLogReaderActive={isLogReaderActive}
-          setActivePage={setActiveSection}
-          openNewSessionModal={() => setIsSessionModalActive(true)}
-          openNewInstanceModal={() => setIsInstanceModalActive(true)}
-          toggleStreamWindow={() => toggleStreamWindow()}
-          toggleSidebar={() => toggleSidebar()}
-        />
-      </div>
+      <Sidebar
+        active={activeSection}
+        setActivePage={setActiveSection}
+      />
 
       <div className="main-content with-sidebar">
         {activeSection === 'current' && <Current isLogReaderActive={isLogReaderActive} />}
         {activeSection === 'history' && <History loadSessionInstance={loadSessionInstance} />}
-      </div>
-
-      <div className={`modal ${isSessionModalActive ? 'is-active' : ''}`}>
-        <div className="modal-background" />
-        <div className="modal-content">
-          <div className="box">
-            <div className="block">
-              <h2 className="title">Start a new session</h2>
-              <p>This will stop any current session.</p>
-            </div>
-            <div className="buttons">
-              <button type="button" className="button is-danger" onClick={startNewSession}>Proceed</button>
-              <button type="button" className="button" onClick={() => setIsSessionModalActive(false)}>Cancel</button>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className={`modal ${isInstanceModalActive ? 'is-active' : ''}`}>
-        <div className="modal-background" />
-        <div className="modal-content">
-          <div className="box">
-            <div className="block">
-              <h2 className="title">Start a new instance</h2>
-              <p>This will stop any current session instance.</p>
-            </div>
-            <div className="buttons">
-              <button type="button" className="button is-danger" onClick={() => startNewInstance()}>Proceed</button>
-              <button type="button" className="button" onClick={() => setIsInstanceModalActive(false)}>Cancel</button>
-            </div>
-          </div>
-        </div>
       </div>
     </>
   );
