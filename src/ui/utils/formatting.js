0a59fe1be89d8315cc2cc7e385ccccfb08498208
Slazor <754107+slazor@users.noreply.github.com>
2022-02-18 19:47:04 +0100

    fix: show unlimited hours, not just 24

--- src/ui/utils/formatting.js
+++ src/ui/utils/formatting.js
@@ -2,7 +2,7 @@ const pad = string_ => string_ < 10 ? `0${string_}` : string_;
 
 export const formatTime = seconds => {
   const values = {
-    hours: pad(Math.max(Math.floor(seconds / 3600) % 24, 0)),
+    hours: pad(Math.max(Math.floor(seconds / 3600), 0)),
     minutes: pad(Math.max(Math.floor(seconds / 60) % 60, 0)),
     seconds: pad(Math.max(Math.floor(seconds % 60), 0)),
   };
