540bbd02e7041acd5db55a2946c0a46a1590245b
Slazor <754107+slazor@users.noreply.github.com>
2022-05-01 15:47:31 +0200

    chore: play around with stat boxes

--- src/ui2/main-content.jsx
+++ src/ui2/main-content.jsx
@@ -14,6 +14,8 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Toolbar from '@mui/material/Toolbar';
 
 const MainContent = () => {
   const [selectedTab, setSelectedTab] = useState(0);
@@ -36,60 +38,194 @@ const MainContent = () => {
         </Tabs>
       </AppBar>
       <Box sx={{ p: 2 }}>
+
+        <Box sx={{ my: 2 }}>
+
+          <Typography variant="h6">
+            General
+          </Typography>
+
+          <Grid container spacing={2}>
+            <Grid item xs={3}>
+              <Card>
+                <CardContent sx={{ p: 1, '&:last-child': { pb: 1 } }}>
+                  <Typography variant="body2" color="text.secondary">
+                    Total loot
+                  </Typography>
+                  <Typography variant="subtitle1">
+                    1052.00 PED
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+
+            <Grid item xs={3}>
+              <Card>
+                <CardContent sx={{ p: 1, '&:last-child': { pb: 1 } }}>
+                  <Typography variant="body2" color="text.secondary">
+                    Globals
+                  </Typography>
+                  <Typography variant="subtitle1">
+                    5
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+
+            <Grid item xs={3}>
+              <Card>
+                <CardContent sx={{ p: 1, '&:last-child': { pb: 1 } }}>
+                  <Typography variant="body2" color="text.secondary">
+                    Hall of fame
+                  </Typography>
+                  <Typography variant="subtitle1">
+                    2
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+
+            <Grid item xs={3}>
+              <Card>
+                <CardContent sx={{ p: 1, '&:last-child': { pb: 1 } }}>
+                  <Typography variant="body2" color="text.secondary">
+                    Rare loot
+                  </Typography>
+                  <Typography variant="subtitle1">
+                    1
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          </Grid>
+        </Box>
+
+        <Box sx={{ my: 2 }}>
+
+          <Typography variant="h6">
+            Offensive
+          </Typography>
+
+          <Grid container spacing={2}>
+            <Grid item xs={3}>
+              <Card>
+                <CardContent sx={{ p: 1, '&:last-child': { pb: 1 } }}>
+                  <Typography variant="body2" color="text.secondary">
+                    Damage inflicted
+                  </Typography>
+                  <Typography variant="subtitle1">
+                    80,157 hp
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+
+            <Grid item xs={3}>
+              <Card>
+                <CardContent sx={{ p: 1, '&:last-child': { pb: 1 } }}>
+                  <Typography variant="body2" color="text.secondary">
+                    Shots fired
+                  </Typography>
+                  <Typography variant="subtitle1">
+                    110
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+
+            <Grid item xs={3}>
+              <Card>
+                <CardContent sx={{ p: 1, '&:last-child': { pb: 1 } }}>
+                  <Typography variant="body2" color="text.secondary">
+                    Kill count
+                  </Typography>
+                  <Typography variant="subtitle1">
+                    945
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+
+            <Grid item xs={3}>
+              <Card>
+                <CardContent sx={{ p: 1, '&:last-child': { pb: 1 } }}>
+                  <Typography variant="body2" color="text.secondary">
+                    Hit rate
+                  </Typography>
+                  <Typography variant="subtitle1">
+                    91.28 %
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          </Grid>
+        </Box>
+
+        <Box sx={{ my: 2 }}>
+          <Paper sx={{ width: '100%', mb: 2 }}>
+            <Toolbar
+              sx={{ pl: { sm: 2 }, minHeight: { sm: '48px' } }}
+            >
+              <Typography variant="h6">
+                Loot
+              </Typography>
+            </Toolbar>
+            <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 650 }}>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Name</TableCell>
+                    <TableCell align="right">Amount</TableCell>
+                    <TableCell align="right">Value</TableCell>
+                    <TableCell align="right">Distribution</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  <TableRow
+                    key={0}
+                    hover
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">Shrapnel</TableCell>
+                    <TableCell align="right">12000</TableCell>
+                    <TableCell align="right">12</TableCell>
+                    <TableCell align="right">100%</TableCell>
+                  </TableRow>
+                  <TableRow
+                    key={1}
+                    hover
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">Shrapnel</TableCell>
+                    <TableCell align="right">12000</TableCell>
+                    <TableCell align="right">12</TableCell>
+                    <TableCell align="right">100%</TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Paper>
+        </Box>
+
         <Card>
           <CardContent>
             <Typography gutterBottom variant="h5" component="div">
-              Total loot
+              Random card
             </Typography>
             <Typography variant="body2" color="text.secondary">
-              Text goes here
+              Some text
             </Typography>
           </CardContent>
         </Card>
-
-        <Box sx={{ my: 2 }}>
-          <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                  <TableCell>Name</TableCell>
-                  <TableCell align="right">Amount</TableCell>
-                  <TableCell align="right">Value</TableCell>
-                  <TableCell align="right">Distribution</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                <TableRow
-                  key={0}
-                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                >
-                  <TableCell component="th" scope="row">Shrapnel</TableCell>
-                  <TableCell align="right">12000</TableCell>
-                  <TableCell align="right">12</TableCell>
-                  <TableCell align="right">100%</TableCell>
-                </TableRow>
-                <TableRow
-                  key={1}
-                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                >
-                  <TableCell component="th" scope="row">Shrapnel</TableCell>
-                  <TableCell align="right">12000</TableCell>
-                  <TableCell align="right">12</TableCell>
-                  <TableCell align="right">100%</TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Box>
 
         <Box sx={{ my: 2 }}>
           <Card>
             <CardContent>
               <TextField
                 multiline
-                label="Custom CSS"
+                label="Notes"
                 rows={10}
-                defaultValue="Default Value"
+                defaultValue=""
                 variant="filled"
                 style={{ width: '100%' }}
               />
